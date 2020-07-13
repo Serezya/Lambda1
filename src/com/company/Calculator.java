f44429e5db9ddc338a4e597a0ddc735035620114
Sergey <derbin.s-a@yandex.ru>
2020-07-13 13:05:10 +0300

    Add divisibility check in lambda expression

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,27 +1,20 @@
 package com.company;
 
-import java.util.function.BinaryOperator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
+import java.util.function.*;
 
 public class Calculator {
-    @FunctionalInterface
-    public interface Supplier<T> {
-        T get();
-    }
     static Supplier<Calculator> instance = Calculator::new;
 
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> (x!=0 && y!=0) ? x / y : 1;
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
     Predicate<Integer> isPositive = x -> x > 0;
 
-    Consumer<Integer> println = System.out::println;
+    Consumer<String> println = System.out::println;
 
 }
